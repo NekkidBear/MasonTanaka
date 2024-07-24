9f1394b2cad09df826c07396e1761b567d1f2a29
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-24 13:50:40 -0500

    creating schema and database connection. add .env variables

---
+++ src/db.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
+      
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
